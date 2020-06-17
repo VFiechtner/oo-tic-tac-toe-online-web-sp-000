6df2ba02ada45d2b1b0a9fc6942032d23463878e
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-06-17 23:15:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,18 @@ class TicTacToe
     end
     return turn
   end
+
+  def current_player
+    num_turns = turn_count
+    if num_turns % 2 == 0
+      player = "X"
+    else
+      player = "O"
+    end
+    return player
+  end
   
+      
 
 
 end
